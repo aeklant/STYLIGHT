423586f20b005c6538bee649c335803646a48ee7
Abraham Escalante <aeklant@gmail.com>
2015-01-28 18:36:27 -0600

    conversion for valid numbers (1 through 3999)

--- conversion.py
+++ conversion.py
@@ -1,8 +1,16 @@
 class Conversion:
 
     def __init__(self):
-        self.std_romans = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C",
-                           500: "D", 1000: "M"}
+        # Order matters, so a dictionary would not be the best choice.
+        # We don't want to run the risk of having the values changed in place,
+        # hence the choice of using tuple rather than list.
+        self.std_romans = ((1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+                           (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+                           (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I"))
+
+        # To increase the range of coverage, simply add the values with their
+        # representation to the 'self.std.romans' tuple and update
+        # 'self.max_rom_to_arab' accordingly.
         self.max_rom_to_arab = 3999
 
     def arabic_to_roman(self, arabic):
@@ -16,4 +24,18 @@ class Conversion:
                              % self.max_rom_to_arab)
         else:
             res = []
+            for arab, rom in self.std_romans:
+                times = value//arab
+                if times:
+                    for i in range(times):
+                        res.append(rom)
+                value -= times*arab
             return "".join(res)
+
+
+if __name__ == "__main__":
+    conv = Conversion()
+    for i in range(1, 4000):
+        print i, conv.arabic_to_roman(i)
+
+    print 4000, conv.arabic_to_roman(4000)
