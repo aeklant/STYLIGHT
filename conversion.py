d62fa06e657c44cf306ee707b6a1c702ac1a2907
Abraham Escalante <aeklant@gmail.com>
2015-01-29 00:06:12 -0600

    added support for negative numbers

--- conversion.py
+++ conversion.py
@@ -1,4 +1,4 @@
-class Conversion:
+class Conversion(object):
 
     def __init__(self):
         # Order matters, so a dictionary would not be the best choice.
@@ -13,19 +13,33 @@ class Conversion:
         # 'self.max_rom_to_arab' accordingly.
         self.max_rom_to_arab = 3999
 
-    def arabic_to_roman(self, arabic):
-        # Validations:
-        # The passed parameter must be an integer.
-        # Integer must be positive between 1 and 3999.
-        value = int(arabic)
+    def arabic_to_roman(self, arabic, negatives=False):
+        try:
+            value = int(arabic)
+        except ValueError:
+            raise ValueError("The value must be a number")
+        res = []
 
-        if value <= 0 or value > self.max_rom_to_arab:
+        if value < 0:
+            if negatives:
+                if abs(value) <= 3999:
+                    res.append("-")
+                    value = abs(value)
+                else:
+                    mess = ("The number's absolute value must be %d or lower"
+                            % self.max_rom_to_arab)
+
+                    raise ValueError(mess)
+            else:
+                raise ValueError("Negatives not supported by default, try using"
+                                 + " 'negatives=True' as a parameter")
+
+        if value == 0 or value > self.max_rom_to_arab:
             raise ValueError("Value must be an integer between 1 and %d"
                              % self.max_rom_to_arab)
         else:
-            res = []
             for arab, rom in self.std_romans:
-                times = value//arab
+                times = value//arab     # For 2.X, 3.X compatibility
                 if times:
                     for i in range(times):
                         res.append(rom)
@@ -35,7 +49,4 @@ class Conversion:
 
 if __name__ == "__main__":
     conv = Conversion()
-    for i in range(1, 4000):
-        print i, conv.arabic_to_roman(i)
-
-    print 4000, conv.arabic_to_roman(4000)
+    print ("Hello", conv.arabic_to_roman("Hello"))
