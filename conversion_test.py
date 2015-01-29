61da8923dc883336aaae5b80ddf1c36db2f49343
Abraham Escalante <aeklant@gmail.com>
2015-01-28 18:35:11 -0600

    test cases for valid numbers

--- conversion_test.py
+++ conversion_test.py
@@ -28,5 +28,39 @@ class conversionTest(unittest.TestCase):
     def test_arabic_to_roman_one(self):
         self.assertEqual("XXI", conversionTest.conv.arabic_to_roman(21.0))
 
+    def test_arabic_to_roman_three_thousand_nine_hundred_ninety_nine(self):
+        self.assertEqual("MMMCMXCIX",
+                         conversionTest.conv.arabic_to_roman(3999))
+
+    def test_arabic_to_roman_four(self):
+        self.assertEqual("IV", conversionTest.conv.arabic_to_roman(4))
+
+    def test_arabic_to_roman_five_hundred_seventy_eight(self):
+        self.assertEqual("DLXXVIII", conversionTest.conv.arabic_to_roman(578))
+
+    def test_arabic_to_roman_two(self):
+        self.assertEqual("II", conversionTest.conv.arabic_to_roman(2))
+
+    def test_arabic_to_roman_thirteen(self):
+        self.assertEqual("XIII", conversionTest.conv.arabic_to_roman(13))
+
+    def test_arabic_to_roman_two_hundred_seven(self):
+        self.assertEqual("CCVII", conversionTest.conv.arabic_to_roman(207))
+
+    def test_arabic_to_roman_one_thousand_sixty_six(self):
+        self.assertEqual("MLXVI", conversionTest.conv.arabic_to_roman(1066))
+
+    def test_arabic_to_roman_one_thousand_nine_hundred_fifty_four(self):
+        self.assertEqual("MCMLIV", conversionTest.conv.arabic_to_roman(1954))
+
+    def test_arabic_to_roman_two_thousand_nine_hundred_ninety(self):
+        self.assertEqual("MMCMXC", conversionTest.conv.arabic_to_roman(2990))
+
+    def test_arabic_to_roman_two_thousand_fourteen(self):
+        self.assertEqual("MMXIV", conversionTest.conv.arabic_to_roman(2014))
+
+    def test_arabic_to_roman_eighty_seven(self):
+        self.assertEqual("LXXXVII", conversionTest.conv.arabic_to_roman(87))
+
 if __name__ == "__main__":
     unittest.main()
